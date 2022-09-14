112229f7393aba7d36d1f3d4a59d1cb12ffe356b
denis <denys.minich@gmail.com>
2022-09-14 21:25:23 +0300

    Added showing user name and message

--- web/scripts/message.php
+++ web/scripts/message.php
@@ -1,25 +1,30 @@
 <?php
 
-$name = $_POST['name'];
+$user_id = $_SESSION['user']['id'];
 $message = $_POST['message'];
-$file = "file.txt";
-$user_name = "Ім'я - ";
-$user_message = "Повідомлення - ";
-$Saved_File = fopen($file, 'a+');
-fwrite($Saved_File, $user_name . $message .  "\n" );
-fwrite($Saved_File, $user_message. $message . "\n");
-fwrite($Saved_File, date("F j, Y, g:i a") ."\n"."\n");
-fclose($Saved_File);
+
 
 $db = db_connect();
 
 $date = date('Y-m-d h:m:s');
-if ($result = $db->query("SELECT * FROM `messages` WHERE `name` = '$name'")) {
-    $user = $result->fetch_assoc();
-    $db->query("INSERT INTO `messages` (`name`, `message`, `createtime_message`) VALUES ( '$name', '$message', '$date')");
+if ($tweet = $db->query("SELECT * FROM `messages` WHERE `message` = '$message'")) {
+    $message_row = $tweet->fetch_assoc();
+    $db->query("INSERT INTO `messages` ( `user_id`, `message`, `createtime_message`) VALUES ( '$user_id', '$message', '$date')");
 } else {
     echo db_error(__LINE__ . ':' . __FILE__, $db);
 }
+
+if ($user_message_array = $db->query("SELECT * FROM `messages` WHERE `user_id` = '$user_id' ORDER BY `id` DESC LIMIT 3")) {
+    $show_user_message = $user_message_array->fetch_assoc();
+}
+
+if ($user_name_array = $db->query("SELECT `id`, `name` FROM `users` WHERE `id` = '$user_id'")) {
+    $show_user_name = $user_name_array->fetch_assoc();
+}
+
+
+$user_name = $show_user_name['name'];
+$user_message = $show_user_message['message'];
 
 $db->close();
 
